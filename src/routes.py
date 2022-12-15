76be044657ffa822244fb08ea1d82638feeb592d
MrHoss <danielsouzaT99@hotmail.com>
2022-12-15 00:53:13 -0300

    Update(v0.0.4)

--- src/routes.py
+++ src/routes.py
@@ -4,6 +4,8 @@ from src.services.interfaceController import InterfaceController
 
 app = Flask(__name__, template_folder='templates')       
 Role = "tutor"
+def errorPage404():
+    return render_template('pages/404.jinja', title = '404',role=Role), 404
     
 #EDUCA++ ROUTES
 @app.route('/',methods=['GET'])
@@ -13,10 +15,18 @@ def home():
 @app.route('/<urlRoute>/',methods=['GET'])
 def MainPages(urlRoute):
     Interface = InterfaceController.controllerMainPgs(urlRoute)
-    print(Interface)
-    return render_template('pages/{}.jinja'.format(urlRoute),interface='Interface',title=urlRoute,role=Role)
+    try:
+        return render_template('pages/{}.jinja'.format(urlRoute),interface=Interface,title=urlRoute,role=Role)
+    except:
+        return errorPage404()
 
-
+@app.route('/<string:urlRoute>/<string:urlRoute2>/',methods=['GET'])
+def InsidePages(urlRoute,urlRoute2):
+    Interface = InterfaceController.controllerInsidePgs(f"{urlRoute}/{urlRoute2}")
+    try:
+        return render_template('pages/{}/{}.jinja'.format(urlRoute,urlRoute2),interface=Interface,title=urlRoute,role=Role)
+    except:
+        return errorPage404()
 
 #END OF EDUCA++ ROUTES
 
@@ -31,9 +41,9 @@ def quizzppPages(urlRoute):
     
 #END OF QUIZZPP ROUTES
 
-"""@app.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(error):
-   return errorPage404()"""
+   return errorPage404()
    
 
 @app.route('/api')
